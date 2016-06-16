4d35db770a00b9ac02be9571cde1e947f9867c4d
Paulo <paulo.comp@alu.ufc.br>
2016-06-16 19:59:51 -0300

    Changes to Calculadora
    Pequeno bug na calculadora resolvido; agora, depois de exibir o
    resultado de uma operação ao apertar ‘=‘, o próximo número pressionado
    ignorará o que estiver na tela, como acontece numa calculadora comum.

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,6 +1,12 @@
+var lastInput = "";
+
 $(document).ready(function() {
 	$("input:button").click(function(){
 	//quando um input do tipo button é clicado, a função é chamada
+		if(lastInput == "=") {
+	    	$("#tela").val("");
+	    }
+	    lastInput = ""
 	    var valor = $(this).val(); //recebe o valor do botão
 	    var atual = $("#tela").val(); //recebe o valor que estiver na tela
 	    if(valor=="C"){
@@ -10,6 +16,7 @@ $(document).ready(function() {
 	        if(valor=="="){
 	        //calcula a operação na tela ao pressionar o botão =
 	            $("#tela").val(eval(atual));
+	            lastInput = "=";
 	        }else{
 	        //concatena o valor do botão com o que já existe na tela
 	            $("#tela").val(atual + valor);
