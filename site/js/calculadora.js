1c8d3d58f1d2e8bb544d777eef4c3805ec1a5afe
pauloanaximandro <pauloanaximandrotavares@Paulos-MacBook-Pro.local>
2018-02-21 20:42:35 -0300

    Site Estágio 3

---
+++ site/js/calculadora.js
@@ -0,0 +1,25 @@
+var lastInput = ""; // variável global que guarda o último input
+$(document).ready(function() {
+	$("input:button").click(function(){
+	//quando um input do tipo button é clicado, a função é chamada
+		if(lastInput == "=") {
+	    	$("#tela").val("");
+	    }
+	    lastInput = ""
+	    var valor = $(this).val(); //recebe o valor do botão
+	    var atual = $("#tela").val(); //recebe o valor que estiver na tela
+	    if(valor=="C"){
+	    //zera a tela ao pressionar o botão C
+	        $("#tela").val("");
+	    }else{
+	        if(valor=="="){
+	        //calcula a operação na tela ao pressionar o botão =
+	            $("#tela").val(eval(atual));
+	            lastInput = "=";
+	        }else{
+	        //concatena o valor do botão com o que já existe na tela
+	            $("#tela").val(atual + valor);
+	        }
+	    }
+	});
+});
